b3634c483e026c7ac816de91d3d8b803fb2158a4
Lucas Katt <kattlucas1@gmail.com>
2016-03-21 21:28:17 -0400

    restructure

---
+++ application/gifquiz.py
@@ -0,0 +1,19 @@
+from flask import *
+#from flask.ext.mail import Mail
+
+
+app = Flask(__name__, template_folder='views')
+
+#mail = Mail()
+#app.config['DEBUG'] = True
+#app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+#app.config['MAIL_PORT'] = 465
+#app.config['MAIL_USE_SSL'] = True
+#app.config['MAIL_USERNAME'] = '485Group71@gmail.com'
+#app.config['MAIL_PASSWORD'] = 'GradeABeef'
+#mail.init_app(app)
+
+
+import application.controllers.main
+
+app.register_blueprint(controllers.main)
